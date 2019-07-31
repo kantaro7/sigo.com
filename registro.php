779f0fdaba4daa26c186b64a83bc10d6833219aa
Kantaro <jesusreyes184@gmail.com>
2019-07-31 14:20:16 -0400

    sfd

--- registro.php
+++ registro.php
@@ -36,8 +36,7 @@
 <html lang="es">
 
 <head>
-
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
   <title>SIGO</title>
 
